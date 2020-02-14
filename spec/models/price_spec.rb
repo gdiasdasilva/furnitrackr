d29cebbf4056110c6a18a8e2ffe79b3b1712d031
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2020-02-14 19:58:24 +0000

    Add should_matchers; Add price spec

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(
+      product: create(:product),
+      value: 500,
+    )
+  end
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a product" do
+    subject.product = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without value" do
+    subject.value = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with value < 1" do
+    subject.value = 0
+    expect(subject).to_not be_valid
+  end
+
+  describe "associations" do
+    it { should belong_to :product }
+  end
 end
