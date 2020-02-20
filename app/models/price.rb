73d22c576c4aca27dc1802a502933590aa49d7d9
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2020-02-20 00:47:27 +0000

    Add chartkick and chart to tracker show page

--- app/models/price.rb
+++ app/models/price.rb
@@ -3,4 +3,8 @@ class Price < ApplicationRecord
 
   validates :product_id, presence: true
   validates :value, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def self.group_by_day
+    order("date_trunc('day', prices.created_at)").pluck("date_trunc('day', prices.created_at)", :value)
+  end
 end
