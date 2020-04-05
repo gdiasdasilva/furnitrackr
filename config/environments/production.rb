8500f162c1fa1c5a03abf07c0a18206c25f0f3bb
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2020-04-05 19:29:23 +0100

    Add sendgrid configuration to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => "furnitrackr.com" }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "apikey",
+    password: ENV["SENDGRID_API_KEY"],
+    domain: "furnitrackr.com",
+    address: "smtp.sendgrid.net",
+    port: 465,
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
