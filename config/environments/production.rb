fa2482dca28748318e17924e1d1d238b36fd86f4
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2020-04-05 18:47:38 +0100

    Uncomment prod default host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "furnitrackr_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => "furnitrackr.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -109,5 +110,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  # config.action_mailer.default_url_options = { :host => "furnitrackr.com" }
 end
