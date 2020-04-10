6131ebd719d9636d718d74bae5724b52e2ec287b
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2020-04-10 18:32:56 +0100

    Improve mailer links

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ApplicationMailer
   def price_drop_notification
     @user    = params[:user]
     @tracker = params[:tracker]
-    @url     = root_url
 
     mail(to: @user.email, subject: "Furnitrackr | Price has dropped!")
   end
