690710ec4686ea9e7392270d99be85fe8c8a23dd
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2020-04-12 01:27:42 +0100

    Fix contact submission job params bug

--- app/jobs/send_user_contact_submission_job.rb
+++ app/jobs/send_user_contact_submission_job.rb
@@ -2,6 +2,6 @@ class SendUserContactSubmissionJob < ApplicationJob
   queue_as :user_contact_submissions
 
   def perform(message, email_address)
-    UserMailer.user_contact_submission(message, email: email_address).deliver_now
+    UserMailer.user_contact_submission(message, email_address).deliver_now
   end
 end
